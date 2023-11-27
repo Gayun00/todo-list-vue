7e9ae6c9779cd5657c2ac64c22a5ba34391413f7
Gayun00 <gayunlee11@gmail.com>
2023-11-27 16:14:39 +0900

    Add: 목록 탭 - 카테고리 ui 추가

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,6 +1,31 @@
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+</script>
+
 <template>
   <div>
-    <h1>This is an list page</h1>
+    <div class="w-40">
+      <Select>
+        <SelectTrigger>
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="title"> 제목 </SelectItem>
+            <SelectItem value="description"> 설명 </SelectItem>
+            <SelectItem value="status"> 상태 </SelectItem>
+            <SelectItem value="date"> 날짜 </SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
   </div>
 </template>
 
