d7748617992b2056440586ee7bcf681d95824c3a
Gayun00 <gayunlee11@gmail.com>
2023-11-27 17:18:09 +0900

    Add: category 선택 값 vuex로 관리
    - vuex 설정

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -11,12 +11,33 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 </script>
 
+<script lang="ts">
+export default {
+  data() {
+    return {
+      selectedValue: 'title'
+    }
+  },
+  watch: {
+    selectedValue(newVal) {
+      this.handleSelectionChange(newVal)
+    }
+  },
+  methods: {
+    handleSelectionChange(value: string) {
+      this.$store.commit('updateItemFilter', { category: value })
+      console.log(this.$store.state.category)
+    }
+  }
+}
+</script>
+
 <template>
   <div class="flex gap-x-3 w-full max-w-2xl min-w-max">
     <div class="w-40">
-      <Select>
+      <Select v-model="selectedValue">
         <SelectTrigger>
-          <SelectValue placeholder="Select a fruit" />
+          <SelectValue placeholder="제목" />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
