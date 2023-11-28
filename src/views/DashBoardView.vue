b833792e04a4ac84d0c06e7258ff92b3869174e3
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-28 23:18:54 +0900

    Add: 통계탭 > Line chart 추가 (#19)

--- src/views/DashBoardView.vue
+++ src/views/DashBoardView.vue
@@ -1,7 +1,72 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { mockTodoList } from '@/mocks/data'
+import type { TodoItem } from '@/types'
+import ApexCharts from 'apexcharts'
+import { onMounted } from 'vue'
+import dayjs from 'dayjs'
+
+const genrerateRecentDates = () => {
+  const dates: Record<string, number> = {}
+  const today = dayjs()
+
+  for (let i = 0; i < 7; i++) {
+    dates[today.subtract(i, 'day').format('YYYY-MM-DD')] = 0
+  }
+
+  return dates
+}
+
+const countTodoPerDate = (todoList: TodoItem[]) => {
+  const recentDates = genrerateRecentDates()
+  for (const todo of todoList) {
+    const todoDate = dayjs(todo?.date).format('YYYY-MM-DD')
+    recentDates[todoDate]++
+  }
+  return recentDates
+}
+
+const dates = countTodoPerDate(mockTodoList)
+
+const options = {
+  series: [
+    {
+      name: 'Desktops',
+      data: Object.values(dates)
+    }
+  ],
+  chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: '일별 todo 갯수',
+    align: 'left'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'],
+      opacity: 0.5
+    }
+  },
+  xaxis: {
+    categories: Object.keys(dates)
+  }
+}
+onMounted(() => {
+  var chart = new ApexCharts(document.querySelector('#line-chart'), options)
+  chart.render()
+})
+</script>
 
 <template>
-  <main>
-    <p>dash board</p>
-  </main>
+  <div id="line-chart" class="w-full"></div>
 </template>
