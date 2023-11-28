3f3e05c51d8fa0f47ee1dbee2154f9064a6d698e
Gayun00 <gayunlee11@gmail.com>
2023-11-28 23:15:17 +0900

    Add: 통계탭 > Line chart 추가

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -1,44 +1,58 @@
-export const mockTodoList = [
+import type { TodoItem } from '@/types'
+
+export const mockTodoList: TodoItem[] = [
   {
     title: 'card1',
     description: 'card 1 description',
-    date: 'date',
+    date: '2023-11-28',
     status: 'inprogress'
   },
   {
     title: 'card2',
     description: 'card 2 description',
-    date: 'date',
+    date: '2023-11-26',
     status: 'inprogress'
   },
   {
     title: 'card2',
     description: 'card 2 description',
-    date: 'date',
+    date: '2023-11-22',
     status: 'inprogress'
   },
   {
     title: 'card2',
     description: 'card 2 description',
-    date: 'date',
+    date: '2023-11-24',
     status: 'inprogress'
   },
   {
     title: 'card2',
     description: 'card 2 description',
-    date: 'date',
+    date: '2023-11-23',
     status: 'inprogress'
   },
   {
     title: 'card2',
     description: 'card 2 description',
-    date: 'date',
+    date: '2023-11-28',
     status: 'inprogress'
   },
   {
     title: 'card2',
     description: 'card 2 description',
-    date: 'date',
+    date: '2023-11-25',
+    status: 'inprogress'
+  },
+  {
+    title: 'card2',
+    description: 'card 2 description',
+    date: '2023-11-24',
+    status: 'inprogress'
+  },
+  {
+    title: 'card2',
+    description: 'card 2 description',
+    date: '2023-11-24',
     status: 'inprogress'
   }
 ]
